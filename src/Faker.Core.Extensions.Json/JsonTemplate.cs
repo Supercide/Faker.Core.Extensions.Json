0633a800075db88a3c43b378fdc6e187d259943b
Jordan anderson <jordan_anders@live.co.uk>
2017-10-27 00:34:17 +0100

    Removes json tamplate
    not json specific

--- src/Faker.Core.Extensions.Json/JsonTemplate.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Faker.Core.Extensions.Json {
-    public class JsonTemplate : ITemplate
-    {
-        public IRequest Request { get; set; }
-        public IResponse Response { get; set; }
-    }
-}
